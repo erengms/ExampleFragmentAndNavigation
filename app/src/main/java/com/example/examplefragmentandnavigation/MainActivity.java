a2a2e4ed19a8ad416f74074bc1530ae6d962697c
Eren <eren.gumus@idb.jsb>
2021-11-11 10:47:46 +0300

    Create menu

--- app/src/main/java/com/example/examplefragmentandnavigation/MainActivity.java
+++ app/src/main/java/com/example/examplefragmentandnavigation/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.examplefragmentandnavigation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.examplefragmentandnavigation.databinding.ActivityMainBinding;
@@ -18,5 +22,19 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.art_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        //Todo navigation graph
+        return super.onOptionsItemSelected(item);
     }
 }
